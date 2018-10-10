c002c2df71a40e8ec5ea1d9c95b1628209106bf5
Robin <robin.narlely@wipro.com>
2018-10-10 13:34:01 +0530

    part 1 hands on excercise

---
+++ hands_on_exercise.py
@@ -0,0 +1,46 @@
+"""Intro to Python - Part 1 - Hands-On Exercise."""
+
+
+import math
+import random
+
+
+# TODO: Write a print statement that displays both the type and value of `pi`
+pi = math.pi
+print (type(pi),pi)
+
+
+# TODO: Write a conditional to print out if `i` is less than or greater than 50
+i = random.randint(0, 100)
+if i < 50:
+    print ("i is less than 50")
+elif i > 50:
+    print ("i is greater than 50" )
+else:
+    print ("i is equal to 50")
+
+
+# TODO: Write a conditional that prints the color of the picked fruit
+picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
+if picked_fruit == 'orange':
+    print("The picked fruit is", picked_fruit, "and its orange in colour")
+elif picked_fruit == 'strawberry':
+    print("The picked fruit is", picked_fruit, "and its pink in colour")
+elif picked_fruit == 'banana':
+    print("The picked fruit is",picked_fruit, "and its yellow in colour")
+    
+# TODO: Write a function that multiplies two numbers and returns the result
+# Define the function here.
+def multiply_two_number (number1,number2):
+    answer=number1*number2
+    return answer
+z = multiply_two_number(12,3)
+print(z)
+
+# TODO: Now call the function a few times to calculate the following answers
+print("12 x 96 =", multiply_two_number (12,96) )
+
+print("48 x 17 =",multiply_two_number (48,17))
+
+print("196523 x 87323 =",multiply_two_number (196523,87323))
+
