6efc531ab956115f452bcfa8a2fd8068c98da6c7
Robin <robin.narlely@wipro.com>
2018-10-11 08:42:47 +0530

    Update Python-Part1-Practice.py

--- Python-Part1-Practice.py
+++ Python-Part1-Practice.py
@@ -29,7 +29,7 @@ print (result)
 
 def positive(n):
     if(n>=0):
-      print("The numbers are given below ")
+      print("The numbers are as below ")
       s = 0
       n_str = str(n)
       while (n > 0):
@@ -39,16 +39,70 @@ def positive(n):
         s = s + 1
       return s
     else:
-        print("the entered number is not positive")
-v1 = int(input("Enter a positive Number:" ))
+        print("the number entered is not positive")
+v1 = int(input("Input a positive Number:" ))
 count = positive(v1)
-print("no of iteration used is ",count)
+print("number of iteration used is ",count)
 
 
 
 # Write a Python program to find the digits which are absent in a given mobile number
 
+a1 = []
+a = int(input("Enter a mobile number (only 10 digit): "))
+c = 0
+while (c <= 9):
+    a1.append(a % 10) 
+    a = int ( a / 10 )
+    c = c + 1
+
+n = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for i in a1:
+    for j in n:
+        if i == j:
+            k=int(i)
+            b[k] = True
+l = 0
+for m in b:
+    if m == False:
+       print(n[l],"is not present")
+    l += 1
+
+
+
 #Write a Python program to create all possible strings by using 'a', 'e', 'i', 'o', 'u'. Use the characters exactly once
 
+import itertools  
+var = "aeiou"
+for i in itertools.permutations(var):
+    print(i)
+
+
+
 # Write a Python program to find common divisors between two numbers in a given pair
+
+a =int(input("Input first number"))
+b =int(input("Input second number"))
+if a > b:
+    g = a
+    l = b
+    if a % b == 0:
+        print(b,"is a common divisor")
+    i = 1
+    while (i <= int(b/2)):
+        if ((g % i == 0) and (l % i == 0)):
+            print(i,"is a common divisor")
+        i = i + 1
+else:
+    g = b
+    l = a
+    if b % a == 0:
+        print(a,"is a common divisor")
+    i = 1
+    while (i <= int(a/2)):
+        if ((g % i == 0) and (l % i == 0)):
+            print(i,"is a common divisor")
+        i = i + 1
 
