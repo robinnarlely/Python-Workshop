c60fd71a95c8c02aadb9aea779c53fea1d910575
Robin <robin.narlely@wipro.com>
2018-10-11 10:24:38 +0530

    Update fortune_cookie.py

--- fortune_cookie.py
+++ fortune_cookie.py
@@ -40,8 +40,7 @@ def create_fortune_cookie_message(how_many_lucky_numbers: int) -> str:
     lucky_numbers_list = generate_lucky_numbers(how_many_lucky_numbers)
     lucky_numbers = ','.join(str(e) for e in lucky_numbers_list)
     return fortune_message + '\nYour lucky numbers are :' + lucky_numbers
-    
-    raise NotImplementedError()
+
 
 def main():
     """Create and print a fortune cookie."""
